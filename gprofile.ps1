ef6e71a2167d89be7c9a53a513b55f6de8dc41bb
eoq <eoq@users.noreply.github.com>
2019-07-21 20:45:36 -0400

    test online change

--- gprofile.ps1
+++ gprofile.ps1
@@ -1,5 +1,6 @@
 ##############################################################################
-### This profile is downloaded by the actual local profile - lprofile ($profile)
+### This profile is downloaded by the actual local profile - 
+###	lprofile ($profile)
 ###
 #create a new profile
 #new-item -itemtype file -force $profile
